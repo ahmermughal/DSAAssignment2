029e79e56daa9a72e4b4a87bfed5950faef36496
Ahmer Mughal <a2mughal143@gmail.com>
2020-04-02 16:08:31 +0500

    Initial Commit

---
+++ DSAAssignment2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DSAAssignment2
+//
+//  Created by Ahmer Mughal on 02/04/2020.
+//  Copyright © 2020 iDevelopStudio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
